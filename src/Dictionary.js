4ea5fe28cf295405f2dad840e5a65fb4b83cd1c3
M-i-a-S <mia.stajic@gmail.com>
2021-09-14 11:31:14 +0200

    Added Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [searchWord, setSearchWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function handleSearch(event) {
     event.preventDefault();
-    alert(`Searching for ${searchWord}`);
+    
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleSearchWord(event) {
     setSearchWord(event.target.value);
