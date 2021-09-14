afef47bc1ddf182830ee5ed2718f22506064cd32
M-i-a-S <mia.stajic@gmail.com>
2021-09-14 13:16:31 +0200

    Updated example component

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,6 +9,7 @@ export default function Meaning(props) {
           <div key={index}>
             <p>
               {definition.definition}
+              <br />
               <em>{definition.example}</em>
             </p>
           </div>
