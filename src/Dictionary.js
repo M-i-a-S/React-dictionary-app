823f98c9a3d038def742e2a9eebc206d2d92b3c5
M-i-a-S <mia.stajic@gmail.com>
2021-09-14 13:09:53 +0200

    Rendered word meaning, definitions and examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [searchWord, setSearchWord] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function handleSearch(event) {
     event.preventDefault();
-    
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
     axios.get(apiUrl).then(handleResponse);
@@ -24,6 +25,7 @@ export default function Dictionary() {
       <form className="search-bar" onSubmit={handleSearch}>
         <input type="search" onChange={handleSearchWord} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
