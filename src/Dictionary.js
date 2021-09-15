e5c886428246953ec8cec69a0d38cc6efd0bf778
M-i-a-S <mia.stajic@gmail.com>
2021-09-15 10:22:50 +0200

    Set styling of the application

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,29 +3,52 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [searchWord, setSearchWord] = useState("");
+export default function Dictionary(props) {
+  const [searchWord, setSearchWord] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function handleSearch(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleSearchWord(event) {
     setSearchWord(event.target.value);
   }
-  return (
-    <div className="Dictionary">
-      <form className="search-bar" onSubmit={handleSearch}>
-        <input type="search" onChange={handleSearchWord} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1> What are you looking for today? </h1>
+          <form className="search-bar" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Type a word"
+              onChange={handleSearchWord}
+            />
+          </form>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading..";
+  }
 }
