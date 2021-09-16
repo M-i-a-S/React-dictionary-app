7d6580b688e82c4e97196906e79360f35ba38373
M-i-a-S <mia.stajic@gmail.com>
2021-09-16 22:31:53 +0200

    Improved sound icon features

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
+  let audio = props.phonetic.audio;
+
+  function playAudio() {
+    const audioSound = new Audio(audio);
+    audioSound.play();
+  }
+
   return (
     <div className="Phonetic">
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        className="SoundIcon"
-      >
-        🔊
-      </a>{" "}
+      <span className="SoundIcon" onClick={playAudio}>
+        {" "}
+        🔊{" "}
+      </span>{" "}
       <span className="Text">/{props.phonetic.text}/</span>
     </div>
   );
