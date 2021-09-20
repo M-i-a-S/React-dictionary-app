275f78ef915745a08d0064b8464c98d1933c7144
M-i-a-S <mia.stajic@gmail.com>
2021-09-20 14:23:24 +0200

    Updated synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+  if (props.synonyms.length) {
     return (
       <div className="Synonyms">
         Synonyms:{" "}
